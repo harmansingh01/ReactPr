960c66cbec57e93f8151a0aaa8cb8cb88fcf85f5
harman.singh@etax.com.au <harman.singh@etax.com.au>
2017-04-23 15:04:25 +1000

    deleted modules

--- public/components/Examples.js
+++ public/components/Examples.js
@@ -1,7 +1,19 @@
 import React, {Component} from 'react';
+import {Link, IndexLink} from 'react-router';
 
 const Examples = () => {
-  return <h3>Examples...</h3>;
+  return <div>
+    <h1 className="text-center">Examples</h1>
+    <p>Here are a few example locations to try out</p>
+    <ol>
+      <li>
+        <Link to='/?location=Dasuya'>Dasuya, Punjab</Link>
+      </li>
+      <li>
+        <Link to='/?location=Shimla'>Shimla, Himachal Pradesh</Link>
+      </li>
+    </ol>
+  </div>
 };
 
 export default Examples;
--- public/components/main.js
+++ public/components/main.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Nav from "./Nav";
 
-const Main =(props)=>{
-      return  <div>
-          <Nav/>
-          <h2>THis is main component...</h2>
-          {props.children}
-    </div>;
-};
+const Main = (props) => {
+  return <div>
+    <Nav/>
+    <div className="row">
+      <div className="columns medium-6 large-4 small-centered"> {props.children}</div>
+    </div>
 
+  </div>;
+};
 
 export default Main;
