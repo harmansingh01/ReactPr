46e640b748d78547ba1b326878e67b582ff5b453
harman.singh@etax.com.au <harman.singh@etax.com.au>
2017-04-23 15:14:35 +1000

    updated about page

--- public/components/About.js
+++ public/components/About.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 
 const About=()=>{
-  return <h3>About Component...</h3>;
+  return <div>
+    <h1 className="text-center">About</h1>
+    <p>THis is a weatehr application build on React. I have built this for the course</p>
+    <p>Here are some of tools I used</p>
+    <ul>
+      <li>
+        <a href="https://facebook.github.io/react" target="_blank">React</a>This is the javascript framework
+        used
+      </li>
+      <li><a href="https://openweathermap.org" target="_blank">Open Weather Map</a>
+      I used Open Weather map to fetch weather data</li>
+    </ul>
+  </div>;
 };
 
 export default About;
